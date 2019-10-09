9f98fa85e2bdd39259f3e20b025f86944113e307
Vincedream <781376581@qq.com>
2019-10-09 09:44:35 +0800

    feat: 完成react-create-ref note

--- packages/react/src/ReactCreateRef.js
+++ packages/react/src/ReactCreateRef.js
@@ -10,6 +10,7 @@ import type {RefObject} from 'shared/ReactTypes';
 
 // an immutable object with a single mutable value
 export function createRef(): RefObject {
+  // 在 Component 里面使用 React.createRef() 时，仅仅只是返回下面简单对象，等到对应的 DOM 被 render 后，才会将 dom 信息赋值给该对象；
   const refObject = {
     current: null,
   };
