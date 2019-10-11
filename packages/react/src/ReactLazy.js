54e64a28c80bf98b66d2aa4a7a13be4361ad463f
Vincedream <callmevincehua@gmail.com>
2019-10-11 23:55:25 +0800

    feat: add react-lazy note

--- packages/react/src/ReactLazy.js
+++ packages/react/src/ReactLazy.js
@@ -12,9 +12,9 @@ import {REACT_LAZY_TYPE} from 'shared/ReactSymbols';
 export function lazy<T, R>(ctor: () => Thenable<T, R>): LazyComponent<T> {
   return {
     $$typeof: REACT_LAZY_TYPE,
-    _ctor: ctor,
+    _ctor: ctor, // 这是异步加载组件的函数（返回promise）
     // React uses these fields to store the result.
-    _status: -1,
-    _result: null,
+    _status: -1, // 当上面的 promise 发生变化后，status 会发生改变
+    _result: null, // 当 promise resolve 后，返回的结果
   };
 }
